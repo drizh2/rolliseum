e72f2bdc47a9cf8671543dbcb9b47bfe94801fc4
drizh2 <drizh2@ukr.net>
2023-08-21 17:20:10 +0300

    First commit. Initialized Spring project

---
+++ server/src/main/java/com/drizh2/rolliseum/RolliseumApplication.java
@@ -0,0 +1,13 @@
+package com.drizh2.rolliseum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RolliseumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RolliseumApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/drizh2/rolliseum/RolliseumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.drizh2.rolliseum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RolliseumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
